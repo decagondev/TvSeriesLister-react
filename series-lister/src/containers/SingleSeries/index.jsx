0811c7651b7bfda8a4ca96835b61ad6aa1d9d030
decagondev <decagondevelopment@gmail.com>
2018-09-10 07:11:28 +0100

    added a back button to single series component

--- series-lister/src/containers/SingleSeries/index.jsx
+++ series-lister/src/containers/SingleSeries/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import Loader from '../../components/Loader';
+import React from "react";
+import Loader from "../../components/Loader";
+import { Link } from "react-router-dom";
 
 class SingleSeries extends React.Component {
   state = {
@@ -9,30 +10,29 @@ class SingleSeries extends React.Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     fetch(`http://api.tvmaze.com/shows/${id}?embed-episodes`)
-    .then(res => res.json())
-    .then(json => this.setState({ show: json}));
+      .then(res => res.json())
+      .then(json => this.setState({ show: json }));
   }
   render() {
     const { show } = this.state;
 
-    return(
+    return (
       <div>
         {show === null && <Loader />}
-        {
-          show !== null 
-          &&
+        {show !== null && (
           <div>
-            <p>{show.name}}</p>
-            <p>Premiere: {show.premiered}</p>
-            <p>Rating: {show.rating.average}</p>
-            <img src={show.image.medium} alt="show" />
+            <div>
+              <h1>{show.name}}</h1>
+              <p>Premiere: {show.premiered}</p>
+              <p>Rating: {show.rating.average}</p>
+              <img src={show.image.medium} alt="show" />
+            </div>
+            <Link to="/">Back</Link>
           </div>
-
-        }
-
+        )}
       </div>
     );
   }
-};
+}
 
 export default SingleSeries;
