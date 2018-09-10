b5914cee4edac57bed87f33e09a30cc076fb856e
decagondev <decagondevelopment@gmail.com>
2018-09-10 11:16:27 +0100

    added styles to series links

--- series-lister/src/components/SeriesList/index.jsx
+++ series-lister/src/components/SeriesList/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
 import "./index.css";
 
+const StyledListItem = styled.li`
+  padding: 10px;
+  margin: 10px 0;
+  background: rgba(0, 0, 0, 0.5);
+`;
+
 const SeriesListItem = props => (
-  <li>
-    <Link to={`/series/${props.episode.show.id}`}>
+  <StyledListItem>
+    <Link style={{textDecoration: "none", color: "#fff"}}to={`/series/${props.episode.show.id}`}>
       <li key={props.episode.show.id}>{props.episode.show.name}</li>
     </Link>
-  </li>
+  </StyledListItem>
 );
 
 const SeriesList = props => {
